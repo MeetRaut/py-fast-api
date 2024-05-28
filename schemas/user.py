cb2b73f16d6e86fe4aa78ae96f6c93c1fa989a25
Meet Raut <meetraut3004@gmail.com>
2024-05-29 01:23:25 +0530

    update

--- schemas/user.py
+++ schemas/user.py
@@ -9,3 +9,11 @@ def userEntity(item) -> dict:
 def usersEntity(entity) -> list:
     return [userEntity(item) for item in entity]
 
+# Other way
+'''
+def serializeDict(a) -> dict:
+    return {**{i:str(a[i]) for i in a if i=='_id'},**{i:a[i] for i in a if i!='_id'}}
+
+def serializeList(entity) -> list:
+    return [serializeDict(a) for a in entity]
+'''
